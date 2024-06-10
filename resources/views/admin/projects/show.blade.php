e63549cfbdb6d5a47c863840bd80a3f2f4f3fbea
Alfredodaniello <alfredo_daniello@libero.it>
2024-06-10 18:11:20 +0200

    feat add technologies on show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -10,6 +10,11 @@
     @endif
     <div class="mb-1"><strong>Slug:</strong> {{$project->slug}}</div>
     <div class="mb-1"><strong>Type:</strong> {{$project->type ? $project->type->name : 'nessuno'}}</div>
+    <div class="mb-1"><strong>Technology:</strong>
+    @foreach ($project->technologies as $technology)
+        {{$technology->name}}
+    @endforeach
+    </div>
     <div class="mb-3"><strong>Created at:</strong> {{$project->created_at}}</div>
     <p><strong>Sommario:</strong> {{$project->summary}}</p>
 @endsection
